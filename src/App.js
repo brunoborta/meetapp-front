63f1d52c631bd91aeee794968fa3b5a9f039d6d7
Bruno "Borta" Bortagaray <bruno.bortagaray@gmail.com>
2019-08-16 11:05:21 -0300

    First commit. Creating structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
